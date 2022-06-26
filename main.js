5ce62435d4b867dd005099720b0753da4cdfee3d
hackerar31649@gmail.com <hackerar31649@gmail.com>
2022-06-26 14:03:36 +0530

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const init = ()=>{
+  audioBody.play();
+}
 function SharkTank(Aman,Anupam,Ashneer,Namita,Peyush,Vineeta,){
   this.Aman = Aman;
   this.Anupam = Anupam;
@@ -11,38 +14,40 @@ SharkTank = new SharkTank('Hum Bhi Bana Lenge!','Kamal Hai Yar!','Bhai Kya Rha H
 
 const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
 fruits.sort()
-console.log(fruits);
+//console.log(fruits);
 
 
 
 const math = Math.floor(Math.random()*160+1);
-console.log(math);
+//console.log(math);
 const mathTwo = Math.floor(Math.random()*60+1);
-console.log(mathTwo)
+//console.log(mathTwo)
 const mathThree = Math.floor(Math.random()*100/2)*2;
-console.log(mathThree)
+//console.log(mathThree)
 const mathFour = Math.floor(Math.random()*10/2)*2;
-console.log(mathFour)
+//console.log(mathFour)
 const mathFive = Math.floor(Math.random()*100+1);
-console.log(mathFive)
+//console.log(mathFive)
 
 document.getElementById('question').innerHTML = math + "+" + mathTwo + "×" + mathThree + "÷" + mathFour + "-" + mathFive;
 let sum = math + mathTwo * mathThree / mathFour - mathFive;
 console.log(sum)
 function myfuc(){
-  let answer = document.getElementById("myinput").value;
-  if (answer == sum || answer == "Ashneer") {
+  let answer = document.getElementById("myinput").value.toLowerCase();
+  if (answer == sum || answer == "Ashneer" || answer == "infinity") {
     document.querySelector("#para").innerHTML=`Good Job Very Nice Absolutly The Answer Is ${answer} 😉😉`;
     audioPankaj.play();
     console.log(answer);
    newGame.style.display="block";
    imgAman.style.display="block";
+   audioBody.muted=true;
   }
   else{
     document.querySelector("#para").innerHTML=`You Are Failed My Friend The Answer Is Not ${answer} 🥺🥺`;
     audioAshneer.play();
     newGame.style.display="block";
     imgAshneer.style.display="block";
+    audioBody.muted=true;
   }
 }
 
@@ -54,3 +59,4 @@ const imgAman = document.getElementById("imgAman");
 const imgAshneer = document.getElementById("imgAshneer");
 const audioAshneer = new Audio('ashneer.mp3');
 const audioPankaj = new Audio('Pankaj.mp3');
+const audioBody = new Audio('wahran.mp3');
